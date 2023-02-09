699c7d60372f3a9468a5f3359378c01fc3b3cad7
Diego <disorder1977@gmail.com>
2023-02-09 19:07:39 -0300

    Se refactoriza cambio de nombre de clase Controller a clase ControllerPersona subsanando error en el nombre de ese controller

--- src/main/java/com/app/miportfolio/controller/ControllerPersona.java
+++ src/main/java/com/app/miportfolio/controller/ControllerPersona.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Controller {
+public class ControllerPersona {
     
     @Autowired
     private IPersonaService persoServ;
